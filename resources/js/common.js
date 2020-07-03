3b1f6222d25bc819663dba67b3b3d19043c99fa4
hanheesong <gmlthd0908@gmail.com>
2020-07-03 17:13:50 +0900

    반응형 슬라이드 스크립트 추가

--- resources/js/common.js
+++ resources/js/common.js
@@ -1,5 +1,26 @@
-$(function() {
-  $(".js-tab-menu .btn-tab").on("click", function() {
+$(function () {
+  printedYear();
+});
+
+if (window.screen.width) {
+  var setViewport = {
+    setSliderItem: function () {
+      var widthDevice = window.innerWidth || window.screen.width;
+      var widthMin = 560;
+      params = (widthDevice <= widthMin) ? '2' : '4';
+      console.log(widthDevice, widthMin, params);
+
+      //뷰포트 슬라이더
+      $('.js-slider').touchSlider({
+        view: params,
+        gap: 10,
+      });
+    }
+  }
+
+  //탭 슬라이더 초기화
+  $(".js-tab-menu .btn-tab").on("click touchstart", function (e) {
+    e.preventDefault();
     var target = $(this).data("tab");
 
     $(this)
@@ -17,14 +38,15 @@ $(function() {
       .find("#" + target + ".js-tab-contents")
       .show();
 
-      $('.js-slider').touchSlider({
-			view: 2,
-			gap: 10,
-		});
+    $('.js-slider').touchSlider({
+      view: params,
+      gap: 10,
+    });
   });
 
-  printedYear();
-});
+  setViewport.setSliderItem();
+  window.addEventListener("resize", setViewport.setSliderItem);
+}
 
 /* global variable */
 var f = $(".contact-form"),
@@ -38,7 +60,7 @@ function getYear() {
   return year;
 }
 
-function printedYear(){
+function printedYear() {
   var todayYear = getYear();
   $(".year").text(todayYear);
 }
@@ -52,8 +74,8 @@ $(".inp").focusout(function () {
     // If it is not blank. 
     $(this).closest(".inp-wrap").removeClass("error");
   }
-}).focusin(function(){
+}).focusin(function () {
   $(this).closest(".inp-wrap").removeClass("error");
-})
- 
- 
+});
+
+
--- resources/js/greeting.js
+++ resources/js/greeting.js
@@ -1,48 +1,56 @@
 const form = document.querySelector(".js-enter-form"),
   input = form.querySelector(".inp"),
   greeting = document.querySelector(".js-greetings"),
-  intro = document.querySelector(".js-introduce");
+  intro = document.querySelector(".js-introduce"),
+  modal = document.querySelector(".js-modal");
 
 const USER_LS = "currentUser",
-  SHOWING_CN = "active";
+  SHOWING_CN = "active",
+  OUT_CN = "out";
 
 function saveName(text) {
   localStorage.setItem(USER_LS, text);
 }
 
+// enter 이벤트
 function handleSubmit(event) {
   event.preventDefault();
   const currentValue = input.value;
-  paintGreeting(currentValue);
+  paintGreeting(currentValue, status);
   saveName(currentValue);
 }
 
 function askForName() {
-  form.classList.add(SHOWING_CN);
+  modal.classList.add(SHOWING_CN);
   form.addEventListener("submit", handleSubmit);
 }
 
-function paintGreeting(text) {
+function paintGreeting(text, status) {
+  const parent = document.querySelector('.pic');
+  const nodewrap = document.createElement('div');
+  const node = document.createElement('p');
+  const textNode = document.createTextNode(`안녕, ${text} 💛`);
+
   // 폼 숨김
   form.classList.remove(SHOWING_CN);
 
   // 인트로박스 쇼잉
-  greeting.classList.add(SHOWING_CN);
+  modal.classList.remove(SHOWING_CN);
+  modal.classList.add(OUT_CN);
+  greeting.classList.remove(SHOWING_CN);
   intro.classList.add(SHOWING_CN);
 
-  greeting.innerText = `Hello, ${text}💛`;
+  nodewrap.className = 'speech js-speech';
+  nodewrap.appendChild(node);
+  node.appendChild(textNode);
+  parent.appendChild(nodewrap);
+  // greeting.innerText = `반가워요, ${text}💛`;
 
-  $(function() {
-		$('.js-slider').touchSlider({
-			view: 2,
-			gap: 10,
-		});
-  });
+
 }
 
 function loadName() {
   const currentUser = localStorage.getItem(USER_LS);
-  console.log(currentUser);
 
   if (currentUser === null) {
     // not here
@@ -57,3 +65,4 @@ function init() {
 }
 
 init();
+
